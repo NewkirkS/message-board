48738edc88302065fd3475aff008a7f3c6cd6b47
Stephen Newkirk <newkirk771@gmail.com>
2016-10-28 09:48:57 -0700

    Finish save component. Functioning as intended w/ custom form validation in bottom level of component.

--- app/components/new-question.js
+++ app/components/new-question.js
@@ -1,4 +1,23 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  actions: {
+    saveQuestion() {
+      var params = {
+        title: this.get('title'),
+        author: this.get('author'),
+        date: this.get('date'),
+        detail: this.get('detail')
+      };
+      if(params.title === undefined || params.author === undefined || params.date === undefined || params.detail === undefined) {
+        alert("Please fill out all fields");
+      } else {
+        this.sendAction('saveQuestion', params);
+        this.set('title', undefined);
+        this.set('author', undefined);
+        this.set('date', undefined);
+        this.set('detail', undefined);
+      }
+    }
+  }
 });
